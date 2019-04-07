ab8e73eef33a9f86b52c45d98b8c141439453dda
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-04-07 17:00:00 -0600

    Update minesweeper.c

--- minesweeper.c
+++ minesweeper.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "minesweeper.h"
+
+typedef struct _cell cell;
+
+struct _cell {
+  int val;
+  int visiblity;
+}
+
+struct _board {
+  int size;
+  int * data;
+  int bombs;
+  int cleanCells;
+};
