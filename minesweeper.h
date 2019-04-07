c7c0c8d6f5a04d85c3155c0daa9157dbcf7dfa64
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-04-07 16:50:10 -0600

    Update minesweeper.h

--- minesweeper.h
+++ minesweeper.h
@@ -9,6 +9,10 @@ int parseArgs(int argc, char *argv[], char mode[]);
 //frees board off heap at end of game
 void deleteBoard(board * bd);
 
+/* creates board filled with 0's.
+ * mode will determine size and num bombs */
+board * blankBoard(int mode);
+
 //randomly arranges bombs
 board * bombArrangement(board * blank_board);
 
@@ -28,3 +32,17 @@ int printNewBoard(board * startB);
 int revealCell(int row, int col);
 
 #endif
+
+/* Statics:
+ * Get Result (when cell is called, test if bomb or not)
+ *      Like getE
+ * Print Result Message
+ * 
+ *Other Ideas:
+ *Possibly 2 matrices for each board
+ *      1 for true identity (numerical value) and one with 
+ *      1/0 for hidden or visible if hidden corresponding element
+ *      prints as ~
+ *Or can we create a matrix of other objects where each object has 2 values
+ *      Value, and 1/0 for hidden or visible.
+ */
