3569ee69593c7d33828a217bad0f93469dc320f0
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-04-10 16:10:32 -0600

    Update minesweeper.c

--- minesweeper.c
+++ minesweeper.c
@@ -18,12 +18,14 @@ struct _board {
 };
 
 static int getVal(board const *bd, int row, int col){
+  return bd->data[(col-1) * bd->side + row -1].val;
 }
 
 static int setVal(board *bd, int row, int col, int val){
 }
 
 static int getVis(board const *bd, int row, int col){
+  return bd->data[(col-1) * bd->side + row -1].vis;
 }
 
 static int setVis(board *bd, int row, int col, int vis){
