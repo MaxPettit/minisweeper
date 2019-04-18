15ef2af687afa295e2fb89343706ac533efe6585
tiba3907 <49498640+tiba3907@users.noreply.github.com>
2019-04-17 19:55:38 -0600

    Update minesweeper.c

--- minesweeper.c
+++ minesweeper.c
@@ -185,23 +185,98 @@ board * populateCells(board * bomb_bd){
 	cnt=0;
 	for(row=1;row<=bomb_bd->side;row++){
 		for(col=1;row<=bomb_bd->side;col++){
-			if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
-				cnt++;
-			if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
-				cnt++;
-			if(getVal(bomb_bd,row+1,col+1)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row-1,col)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row,col-1)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row-1,col-1)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row-1,col+1)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row+1,col-1)==-1)
-				cnt++;
-			if(getVal(bomb_bd,row,col)!=-1)
+			cnt=0;
+			if(row==bomb_bd->side){
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col+1)==-1)
+					cnt++;
+			}else if(col==bomb_bd->side){
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row+1,col-1)==-1)
+					cnt++;
+			}else if(col==1&&row==1){
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row+1,col+1)==-1)
+					cnt++;
+			}else if(col==bomb_bd->side&&row=1){
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row+1,col-1)==-1)
+					cnt++;
+			}else if(col==bomb_bd->side&&row==bomb_bd->side){
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col-1)==-1)
+					cnt++;
+			}else if(col==1&&row==bomb_bd->side){
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col+1)==-1)
+					cnt++;
+			}else if(row==1){
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row+1,col+1)==-1)
+					cnt++:
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row+1,col-1)==-1)
+					cnt++;
+			}else if(col==1){
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row+1,col+1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col+1)==-1)
+					cnt++;
+			}else {
+				if(getVal(bomb_bd,row,col+1)==-1) //if this fails because there is no check for 
+					cnt++;
+				if(getVal(bomb_bd,row+1,col)==-1) //checks if bombs are anywhere around spot
+					cnt++;
+				if(getVal(bomb_bd,row+1,col+1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row-1,col+1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row+1,col-1)==-1)
+					cnt++;
+				if(getVal(bomb_bd,row,col)!=-1)
+			}
 				setVal(bomb_bd,row,col,cnt);
 		}
 	}
