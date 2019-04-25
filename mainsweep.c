6075cb9cf9335fc50c9991af421ef5e9babe47bc
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-04-25 12:35:49 -0600

    Update mainsweep.c

--- mainsweep.c
+++ mainsweep.c
@@ -26,9 +26,10 @@ int main(int argc, char *argv[]){
       col = (c - 'a')%26 +1;
       if(r <= 0 || col < 1 || r > mode + 5|| col > mode + 5){
 	printf("Try again\n");
+      }else{
+        err = revealCell(b, r, col)
+        if(err == 2) break;
       }
-      err = revealCell(b, r, col)
-      if(err == 2) break;
     }
     //play again newGame = 1;
   };
