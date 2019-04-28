978f78e18ebbf76e83c849493f1dda0876895ceb
MaxPettit <44881529+MaxPettit@users.noreply.github.com>
2019-04-28 12:28:34 -0600

    Update mainsweep.c

--- mainsweep.c
+++ mainsweep.c
@@ -8,8 +8,7 @@
 
 int main(int argc, char *argv[]){
   board *b;
-  char c, s[7];
-  int mode, err, newGame = 1, ret,col, r;
+  int mode, err, newGame = 1;
   mode = 1;
   srand(time(0));
 
@@ -23,41 +22,8 @@ int main(int argc, char *argv[]){
     b = bombArrangement(b,mode);
     b = populateCells(b);
     err = printBoard(b); assert(!err);
-
-    while(1){
-      ret=0;
-      ret = scanf("%c",&c);
-      if(ret==1){ 
-      col = (c - 'a')%26 +1;
-      ret= scanf("%d",&r);
-      if(ret==1){
-      if(r <= 0 || col < 1 || r > mode + 5|| col > mode + 5){
-	printf("Invalid input. Try again\n");
-      }
-      err = revealCell(b, r, col);
-      if(err == 1) printf("Already opened. Try again\n");
-      if(err == 2) break;
-      }
-      }
-    }
-	  
-    ret = scanf("%s", s);
-    if(strcmp("easy", s) == 0){
-      newGame = 1;
-      mode = 1;
-    }
-    if(strcmp("medium", s) == 0){
-      newGame = 1;
-      mode = 2;
-    }
-    if(strcmp("hard", s) == 0){
-      newGame = 1;
-      mode = 3;
-    }
-    if(strcmp("demo", s) == 0){
-      newGame = 1;
-      mode = 4;
-    }
+    mode = readCell(b);
+    if(mode) newGame = 1;
   }
   
   deleteBoard(b);
